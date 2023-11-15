6d2a3f14438b477d4a647164dfbfbcd5bc562ccc
Aanas Aamer <anasamer056@gmail.com>
2023-11-15 23:35:20 +0200

    Create a master function to execute the requested operation based on the operator

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+// Variables 
+let firstOperand;
+let secondOperand;
+let operator;
+
+// Math Logic 
 let add = (a,b) => a+b;
 let subtract = (a,b) => a-b;
 let multiply = (a,b) => a*b;
 let divide = (a,b) => a/b;
 
-console.log("add", add(2,5));
-console.log("subtract", subtract(5, 4))
-console.log("multiply", multiply(3,6));
-console.log("divide", divide(6,7))
+// console.log("add", add(2,5));
+// console.log("subtract", subtract(5, 4))
+// console.log("multiply", multiply(3,6));
+// console.log("divide", divide(6,7))
+
+// Master Function
+function operate (firstOperand, operator, secondOperand) { 
+    let legend = {
+        "+": add,
+        "-": subtract,
+        "*": multiply,
+        "x": multiply,
+        "/": divide,
+    }
+    return legend[operator](+firstOperand, +secondOperand);
+}
+
+console.log("3 + 5 = ", operate("3", "/", "5"))
+
