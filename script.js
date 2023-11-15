1d6fb317bd914ca123c87e7f3cc85c93cf87c14e
Aanas Aamer <anasamer056@gmail.com>
2023-11-15 23:26:40 +0200

    Create the 4 basic functions for math operations

---
+++ script.js
@@ -0,0 +1,9 @@
+let add = (a,b) => a+b;
+let subtract = (a,b) => a-b;
+let multiply = (a,b) => a*b;
+let divide = (a,b) => a/b;
+
+console.log("add", add(2,5));
+console.log("subtract", subtract(5, 4))
+console.log("multiply", multiply(3,6));
+console.log("divide", divide(6,7))
